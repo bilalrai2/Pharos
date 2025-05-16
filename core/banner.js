edb3d00922d7cc1c680ae26a7107833c7168ec03
bilalrai2 <kharalbilal135@gmail.com>
2025-05-16 19:44:53 +0500

    Create banner.js

---
+++ core/banner.js
@@ -0,0 +1,6 @@
+const colors = require("colors");
+function showBanner() {
+  console.log(colors.yellow("TELEGRAM CHANNEL FOR MORE SCRIPT > https://t.me/AllAirdropScript"));
+}
+
+module.exports = { showBanner };
